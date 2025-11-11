36ff18f7b63001bb354b0953ed8b7bbe5e63ef96
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2025-11-11 23:23:47 +0100

    23:23 11/11

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,13 +15,16 @@ public class PlayerController : MonoBehaviour
     private InputAction _moveAction;
     private InputAction _jumpAction;
 
+    private Vector3 _moveInput;
+
     void Awake()
     {
         _controller = GetComponent<CharacterController>();
 
-        _moveAction = _gameplayActionMap.FindAction("Move");
-        _jumpAction = _gameplayActionMap.FindAction("Jump");
+        /*_moveAction = _gameplayActionMap.FindAction("Move");
+        _jumpAction = _gameplayActionMap.FindAction("Jump");*/
 
+        _moveAction = InputSystem.actions["Move"];
         _jumpAction = InputSystem.actions["Jump"];
     }
     
@@ -29,6 +32,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        _moveInput = _moveAction.ReadValue<Vector3>();
+        Debug.Log(_moveInput);
+        
         if (_jumpAction.WasPressedThisFrame())
         {
             Debug.Log("Jump");
